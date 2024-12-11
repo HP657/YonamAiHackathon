9a61a9ee1358d92ac99a20029d86ef6db5d25f30
HP657 <junlee5070@gmail.com>
2024-12-11 17:25:25 +0900

    기본 프로젝트 설정

---
+++ YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/Hp657Application.java
@@ -0,0 +1,13 @@
+package com.yonamhackathon.HP657;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hp657Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hp657Application.class, args);
+	}
+
+}
---
+++ YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/global/common/Index/IndexController.java
@@ -0,0 +1,12 @@
+package com.yonamhackathon.HP657.global.common.Index;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "Hello World!";
+    }
+}
