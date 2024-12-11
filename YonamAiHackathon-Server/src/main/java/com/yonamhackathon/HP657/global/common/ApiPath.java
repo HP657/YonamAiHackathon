91b2160dfbb7222728ade8497a935ee415fffb34
HP657 <junlee5070@gmail.com>
2024-12-11 17:25:05 +0900

    API 주소값 설정

---
+++ YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/global/common/ApiPath.java
@@ -0,0 +1,8 @@
+package com.yonamhackathon.HP657.global.common;
+
+public class ApiPath {
+    public static final String BASE_PATH = "/api";
+    public static final String H2_PATH = "h2-console";
+    public static final String USER_API_PATH = BASE_PATH + "/user";
+    public static final String ADMIN_API_PATH = BASE_PATH + "/admin";
+}
