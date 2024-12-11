6e3b213b2aab20b60ec345b9d9f581812e41325a
HP657 <junlee5070@gmail.com>
2024-12-11 17:24:22 +0900

    유저 엔티티

---
+++ YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.yonamhackathon.HP657.domain.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "이메일 형식이 올바르지 않습니다")
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+}
