eabd185b4012330d6d34c16aa65028380f62ebe1
HP657 <junlee5070@gmail.com>
2024-12-11 19:27:59 +0900

    H2 주소 동적 변경

--- YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/global/common/ApiPath.java
+++ YonamAiHackathon-Server/src/main/java/com/yonamhackathon/HP657/global/common/ApiPath.java
@@ -1,8 +1,24 @@
 package com.yonamhackathon.HP657.global.common;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class ApiPath {
     public static final String BASE_PATH = "/api";
-    public static final String H2_PATH = "h2-console";
     public static final String USER_API_PATH = BASE_PATH + "/user";
     public static final String ADMIN_API_PATH = BASE_PATH + "/admin";
+
+
+    public static String H2_PATH;
+
+    @Value("${spring.h2.console.path}")
+    private String h2Path;
+
+    @PostConstruct
+    public void init() {
+        H2_PATH = h2Path;
+    }
 }
