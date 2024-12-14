3877e3560c80d3a09ff6a227d3286cebe9f7851a
HP657 <junlee5070@gmail.com>
2024-12-14 19:21:34 +0900

    회원가입 페이지 라우팅 설정정

--- FrontEnd/src/components/Contents.js
+++ FrontEnd/src/components/Contents.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 
 export default function Contents() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='register' element={<RegisterPage />} />
     </Routes>
   );
 }
