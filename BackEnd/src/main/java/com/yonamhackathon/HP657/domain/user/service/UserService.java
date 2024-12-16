53a72eebb61d0d60965d4974e5cc735383eb8100
HP657 <junlee5070@gmail.com>
2024-12-16 22:56:32 +0900

    토큰으로 학점 데이터 가져오는 서비스

--- BackEnd/src/main/java/com/yonamhackathon/HP657/domain/user/service/UserService.java
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/domain/user/service/UserService.java
@@ -1,20 +1,28 @@
 package com.yonamhackathon.HP657.domain.user.service;
 
+import com.yonamhackathon.HP657.domain.user.dto.ResponseGpaDto;
 import com.yonamhackathon.HP657.domain.user.dto.ResponseRegisterUserDto;
+import com.yonamhackathon.HP657.domain.user.entity.Gpa;
 import com.yonamhackathon.HP657.domain.user.entity.Role;
 import com.yonamhackathon.HP657.domain.user.entity.User;
+import com.yonamhackathon.HP657.domain.user.repository.GpaRepository;
 import com.yonamhackathon.HP657.domain.user.repository.UserRepository;
 import com.yonamhackathon.HP657.global.exception.CustomException;
 import com.yonamhackathon.HP657.global.exception.ErrorCode;
+import com.yonamhackathon.HP657.global.utility.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final GpaRepository gpaRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public ResponseRegisterUserDto save(User user) {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
@@ -26,10 +34,33 @@ public class UserService {
 
         User createdUser = userRepository.save(user);
 
+        Gpa userGpa = new Gpa();
+        userGpa.setEmail(createdUser.getEmail());
+        userGpa.setGpa(3.5);
+        userGpa.setCount(1);
+        gpaRepository.save(userGpa);
+
         return ResponseRegisterUserDto.builder()
                 .userId(createdUser.getUserId())
                 .email(createdUser.getEmail())
                 .build();
     }
+
+    public ResponseGpaDto getUserGpa(String token) {
+        String userEmail = jwtUtil.extractEmail(token);
+
+        Optional<Gpa> optionalGpa = gpaRepository.findByEmail(userEmail);
+
+        if (optionalGpa.isPresent()) {
+            Gpa gpa = optionalGpa.get();
+            ResponseGpaDto responseGpaDto = new ResponseGpaDto();
+            responseGpaDto.setGpa(gpa.getGpa());
+            responseGpaDto.setCount(gpa.getCount());
+            return responseGpaDto;
+        } else {
+            throw new RuntimeException("GPA not found for user: " + userEmail);
+        }
+    }
+
 
 }
