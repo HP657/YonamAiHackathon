0dd014ddeed7a7773868782d3c28f35b78d87d4c
HP657 <junlee5070@gmail.com>
2024-12-17 23:23:41 +0900

    룸 만들떄 쓰는 DTO

---
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/domain/chat/dto/RequestCreateRoomDto.java
@@ -0,0 +1,10 @@
+package com.yonamhackathon.HP657.domain.chat.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestCreateRoomDto {
+    private String roomName;
+    private String discription;
+
+}
