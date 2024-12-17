94e4e66a06fc935dcb896a89dd3b199662fd5710
HP657 <junlee5070@gmail.com>
2024-12-17 23:24:39 +0900

    룸 리포지토리

---
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/domain/chat/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.yonamhackathon.HP657.domain.chat.repository;
+
+import com.yonamhackathon.HP657.domain.chat.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
