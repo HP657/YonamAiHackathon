cc825abe563eaba659cf0810a522785a393db696
HP657 <junlee5070@gmail.com>
2024-12-18 15:22:56 +0900

    체크 다시 해봄

--- BackEnd/src/main/java/com/yonamhackathon/HP657/domain/jwt/controller/JwtController.java
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/domain/jwt/controller/JwtController.java
@@ -3,6 +3,8 @@ package com.yonamhackathon.HP657.domain.jwt.controller;
 import com.yonamhackathon.HP657.domain.jwt.service.JwtService;
 import com.yonamhackathon.HP657.global.common.ApiPath;
 import com.yonamhackathon.HP657.global.common.DefaultController;
+import com.yonamhackathon.HP657.global.utility.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +13,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(ApiPath.JWT_API_PATH)
 public class JwtController extends DefaultController {
 private final JwtService jwtService;
+private final JwtUtil jwtUtil;
 
     @GetMapping("/check")
-    public boolean checkToken() {
-        return true;
+    public boolean checkToken(HttpServletRequest request, @RequestHeader("Authorization") String token) {
+        if (token != null) {
+            String jwt = token.substring(7);
+
+            try {
+                String email = jwtUtil.extractEmail(jwt);
+                return jwtUtil.validateToken(jwt, email);
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
     }
+
 }
