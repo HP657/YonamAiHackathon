e0c7120bd060216ef65bc86d713668d09d8bf1b7
HP657 <junlee5070@gmail.com>
2024-12-18 15:24:02 +0900

    Link to 써서 리다이렉트

--- FrontEnd/src/components/Footer.js
+++ FrontEnd/src/components/Footer.js
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
-    <footer className='bg-gray-200 h-12 p-2 text-center'>Footer content</footer>
+    <footer className='bg-gray-200 h-12 p-2 text-center'>
+      <div className='flex justify-center space-x-8'>
+        <Link to='/' className='text-blue-600 hover:underline'>
+          HOME
+        </Link>
+        <Link to='/mentoring-study' className='text-blue-600 hover:underline'>
+          ROOM
+        </Link>
+        <Link to='/posts' className='text-blue-600 hover:underline'>
+          POSTS
+        </Link>
+      </div>
+    </footer>
   );
 }
