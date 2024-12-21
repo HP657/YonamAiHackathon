c242a4875494a0d5b89f07d0cf79c4512ba66a08
HP657 <junlee5070@gmail.com>
2024-12-21 16:11:20 +0900

    조금 수정

--- BackEnd/src/main/java/com/yonamhackathon/HP657/global/config/WebSocketConfig.java
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/global/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.yonamhackathon.HP657.global.config;
 
-import com.yonamhackathon.HP657.global.common.ApiPath;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +9,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat")
-                .setAllowedOrigins(ApiPath.BASE_PATH)
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
 
@@ -22,4 +22,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
 }
