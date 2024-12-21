35bd3813f807743ff34438d3b24f8204f3c09326
HP657 <junlee5070@gmail.com>
2024-12-21 16:13:54 +0900

    메시지 엔티티

---
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/domain/chat/entity/Message.java
@@ -0,0 +1,34 @@
+package com.yonamhackathon.HP657.domain.chat.entity;
+
+import com.yonamhackathon.HP657.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long messageId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
