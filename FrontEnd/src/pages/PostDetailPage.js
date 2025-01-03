76528ce88bd201900fbaa8b553721bed6c2f3429
HP657 <junlee5070@gmail.com>
2025-01-03 16:30:12 +0900

    게시물 상세 페이지

--- FrontEnd/src/pages/PostDetailPage.js
+++ FrontEnd/src/pages/PostDetailPage.js
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import API from '../services/api';
+import { useNavigate, useParams } from 'react-router-dom';
+import CheckToken from '../components/TokenCheck';
 
-export default function PostDetailPage({ postId }) {
+export default function PostDetailPage() {
+  const { postId } = useParams();
   const [post, setPost] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const dummyPost = {
-      title: 'Understanding React Hooks',
-      author: 'Jane Doe',
-      date: '2023-10-01T12:00:00Z',
-      content:
-        'React Hooks are functions that let you use state and other React features without writing a class. They are a powerful addition to React that allows for more functional programming patterns.',
+    const tokencheck = async () => {
+      const isValidToken = await CheckToken();
+      if (!isValidToken) {
+        navigate('/login');
+        return;
+      }
+      fetchPost();
     };
+    tokencheck();
+  }, [postId, navigate]);
 
-    const fetchPost = () => {
-      setPost(dummyPost);
-    };
-
-    fetchPost();
-  }, [postId]);
+  const fetchPost = async () => {
+    const response = await API(`/api/post/${postId}`, 'GET', null, true);
+    if (response) {
+      setPost(response.data.data);
+    } else {
+      console.error('Failed to fetch post:', response);
+      setPost(null);
+    }
+  };
 
   if (!post) {
-    return <div></div>;
+    return <div>Loading...</div>;
   }
 
   return (
@@ -29,10 +40,10 @@ export default function PostDetailPage({ postId }) {
       <h2 className='text-2xl font-bold'>{post.title}</h2>
       <div className='text-sm text-gray-500 mb-4'>
         <span>
-          {post.author} | {new Date(post.date).toLocaleString()}
+          {post.author} | {new Date(post.createdAt).toLocaleString()}
         </span>
       </div>
-      <p className='text-gray-700'>{post.content}</p>
+      <p className='text-gray-700'>{post.contents}</p>
     </div>
   );
 }
