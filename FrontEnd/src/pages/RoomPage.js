6abc82ed013a101cffa38a6ddcf34e90b1daf4a3
HP657 <junlee5070@gmail.com>
2025-01-03 16:28:31 +0900

    룸 더미 데이터들 지움

--- FrontEnd/src/pages/RoomPage.js
+++ FrontEnd/src/pages/RoomPage.js
@@ -4,35 +4,38 @@ import { Stomp } from '@stomp/stompjs';
 import PropTypes from 'prop-types';
 import RoomUserModal from '../components/RoomUserModal';
 import ScheduleModal from '../components/ScheduleModal';
+import API from '../services/api';
+import { useNavigate, useParams } from 'react-router-dom';
+import CheckToken from '../components/TokenCheck';
+import RoomRequestModal from '../components/RoomRequestModal';
 
-export default function RoomPage({ roomId }) {
+export default function RoomPage() {
+  const { roomId } = useParams();
+  const navigate = useNavigate();
   const [stompClient, setStompClient] = useState(null);
-  const [roomInfo, setRoomInfo] = useState({
-    id: 5,
-    title: 'Web Development Trends',
-    description: 'The latest trends and innovations in web development.',
-    author: 'Charlie Davis',
-    date: '2023-10-05',
-  });
-  const [messages, setMessages] = useState([
-    { sender: 'User1', content: 'Hello everyone!' },
-    { sender: 'User2', content: 'Hi User1!' },
-    { sender: 'User3', content: 'How are you all?' },
-    { sender: 'User1', content: 'I am good, thanks!' },
-    { sender: 'User2', content: 'What about you?' },
-    { sender: 'User3', content: 'Doing well, just busy with work.' },
-    { sender: 'User1', content: 'Same here!' },
-    { sender: 'User2', content: 'Let’s catch up later.' },
-    { sender: 'User3', content: 'Sounds good!' },
-  ]);
+  const [roomInfo, setRoomInfo] = useState(null);
+  const [messages, setMessages] = useState([]);
   const [messageText, setMessageText] = useState('');
   const [isConnected, setIsConnected] = useState(false);
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
   const [isScheduleModalOpen, setIsScheduleModalOpen] = useState(false);
-  const token = localStorage.getItem('accessToken');
+  const [usersInfo, setUsersInfo] = useState([]);
+  const [myInfo, setMyInfo] = useState(null);
+  const [isRoomRequestModalOpen, setIsRoomRequestModalOpen] = useState(false);
+  const [schedule, setSchedule] = useState(null);
 
   useEffect(() => {
-    connectToWebSocket();
+    const tokencheck = async () => {
+      const isValidToken = await CheckToken();
+      if (!isValidToken) {
+        navigate('/login');
+        return;
+      }
+      fetchRoomInfo();
+      connectToWebSocket();
+    };
+    tokencheck();
+
     return () => {
       if (stompClient) {
         stompClient.disconnect();
@@ -41,16 +44,60 @@ export default function RoomPage({ roomId }) {
     };
   }, []);
 
+  const fetchRoomInfo = async () => {
+    try {
+      const [
+        roomResponse,
+        messageResponse,
+        userResponse,
+        userInfoResponse,
+        scheduleResponse,
+      ] = await Promise.all([
+        API(`/api/room/${roomId}`, 'GET', null, true),
+        API(`/api/chat/${roomId}/messages`, 'GET', null, true),
+        API(`/api/room/${roomId}/users`, 'GET', null, true),
+        API(`/api/user/info`, 'GET', null, true),
+        API(`/api/schedules/room/${roomId}`, 'GET', null, true),
+      ]);
+
+      setMessages(messageResponse.data.data);
+      setRoomInfo(roomResponse.data.data);
+      setUsersInfo(
+        userResponse.data.data.filter(
+          (user) => user.userId !== roomResponse.data.data.owner.userId
+        )
+      );
+      setMyInfo(userInfoResponse.data.data);
+      setSchedule(scheduleResponse.data.data || null);
+    } catch (error) {
+      console.error('Error fetching room info:', error);
+    }
+  };
+
+  const fetchSchedule = async () => {
+    try {
+      const scheduleResponse = await API(
+        `/api/schedules/room/${roomId}`,
+        'GET',
+        null,
+        true
+      );
+      setSchedule(scheduleResponse.data.data || null);
+    } catch (error) {
+      console.error('Error fetching schedule:', error);
+    }
+  };
+
   const connectToWebSocket = () => {
     if (isConnected) return;
 
     const socket = new SockJS('http://localhost:8080/ws');
     const client = Stomp.over(socket);
+    const token = localStorage.getItem('accessToken');
 
     client.connect(
       { Authorization: `Bearer ${token}` },
       () => {
-        console.log('Connected to WebSocket');
         setIsConnected(true);
 
         client.subscribe(
@@ -62,6 +109,12 @@ export default function RoomPage({ roomId }) {
               JSON.parse(messageOutput.body),
             ]);
           }
+        );
+
+        client.subscribe(
+          `/topic/room/${roomId}/schedules`,
+          { Authorization: `Bearer ${token}` },
+          fetchSchedule
         );
 
         setStompClient(client);
@@ -78,7 +131,7 @@ export default function RoomPage({ roomId }) {
     }
 
     const message = {
-      sender: 'You',
+      user: { username: `${myInfo.username}` },
       content: messageText,
     };
 
@@ -86,70 +139,113 @@ export default function RoomPage({ roomId }) {
 
     stompClient.send(
       `/app/sendMessage/${roomId}`,
-      { Authorization: `Bearer ${token}` },
+      { Authorization: `Bearer ${localStorage.getItem('accessToken')}` },
       JSON.stringify(message)
     );
 
     setMessageText('');
   };
 
-  const usersInfo = [
-    { name: 'Charlie Davis', email: 'charlie.davis@example.com' },
-    { name: 'Alice Johnson', email: 'alice.johnson@example.com' },
-    { name: 'Bob Smith', email: 'bob.smith@example.com' },
-  ];
+  const handleScheduleUpdated = (newSchedule) => {
+    setSchedule(newSchedule);
+  };
+
+  if (!roomInfo) return <></>;
+
+  const renderRoomInfo = () => (
+    <div className='bg-gray-100 p-4 rounded-lg mb-4'>
+      <h3 className='font-semibold'>{roomInfo.name}</h3>
+      <p>{roomInfo.description}</p>
+      <p className='text-sm text-gray-600'>By {roomInfo.owner.username}</p>
+    </div>
+  );
+
+  const renderSchedule = () => (
+    <div className='bg-yellow-100 p-4 rounded-lg mb-4'>
+      <h3 className='font-semibold'>Scheduled Meetings</h3>
+      {schedule ? (
+        <ul>
+          <li key={schedule.scheduleId} className='mb-2'>
+            <strong>{schedule.title}</strong> - {schedule.date} {schedule.time}
+          </li>
+        </ul>
+      ) : (
+        <p>스케줄이 없습니다.</p>
+      )}
+    </div>
+  );
+
+  const renderMessages = () => (
+    <div className='flex-1 overflow-y-auto border border-gray-300 rounded-lg p-4 mb-4 max-h-96 no-scrollbar'>
+      <div className='messages'>
+        {messages.map((msg, index) => (
+          <div key={index} className='message mb-2'>
+            <strong>{msg.user?.username || msg.room?.owner?.username}:</strong>{' '}
+            {msg.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  const renderActionButtons = () => (
+    <div className='flex-1 flex justify-between mb-2'>
+      {myInfo && roomInfo.owner.userId === myInfo.userId && (
+        <>
+          <button
+            onClick={() => setIsUserModalOpen(true)}
+            className='bg-blue-500 text-white rounded p-2 hover:bg-blue-600 flex-1 mr-2'
+          >
+            유저 정보 보기
+          </button>
+          <button
+            onClick={() => setIsScheduleModalOpen(true)}
+            className='bg-green-500 text-white rounded p-2 hover:bg-green-600 flex-1 ml-2'
+          >
+            일정잡기
+          </button>
+        </>
+      )}
+    </div>
+  );
+
+  const renderMessageInput = () => (
+    <div className='input-group flex items-center mb-4'>
+      <input
+        type='text'
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+        placeholder='Type a message'
+        className='flex-1 border border-gray-300 rounded-l-lg p-2'
+      />
+      <button
+        onClick={handleSendMessage}
+        className='bg-blue-500 text-white rounded-r-lg p-2 hover:bg-blue-600'
+      >
+        Send
+      </button>
+    </div>
+  );
 
   return (
     <div className='flex flex-col max-w-lg mx-auto p-4'>
-      <h2 className='text-xl font-bold mb-4'>Room {roomId}</h2>
-
-      <div className='bg-gray-100 p-4 rounded-lg mb-4'>
-        <h3 className='font-semibold'>{roomInfo.title}</h3>
-        <p>{roomInfo.description}</p>
-        <p className='text-sm text-gray-600'>
-          By {roomInfo.author} on {roomInfo.date}
-        </p>
+      <div className='flex items-center mb-4 justify-between'>
+        <h2 className='text-xl font-bold'>Room {roomId}</h2>
+        {myInfo && roomInfo.owner.userId === myInfo.userId && (
+          <button
+            onClick={() => setIsRoomRequestModalOpen(true)}
+            className='text-black'
+          >
+            룸 요청
+          </button>
+        )}
       </div>
 
-      <div className='flex-1 overflow-y-auto border border-gray-300 rounded-lg p-4 mb-4 max-h-96 no-scrollbar'>
-        <div className='messages'>
-          {messages.map((msg, index) => (
-            <div key={index} className='message mb-2'>
-              <strong>{msg.sender}:</strong> {msg.content}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className='flex-1 flex justify-between mb-2'>
-        <button
-          onClick={() => setIsUserModalOpen(true)}
-          className='bg-blue-500 text-white rounded p-2 hover:bg-blue-600 flex-1 mr-2'
-        >
-          유저 정보 보기{' '}
-        </button>
-        <button
-          onClick={() => setIsScheduleModalOpen(true)}
-          className='bg-green-500 text-white rounded p-2 hover:bg-green-600 flex-1 ml-2'
-        >
-          일정잡기{' '}
-        </button>
-      </div>
-
-      <div className='input-group flex items-center mb-4'>
-        <input
-          type='text'
-          value={messageText}
-          onChange={(e) => setMessageText(e.target.value)}
-          placeholder='Type a message'
-          className='flex-1 border border-gray-300 rounded-l-lg p-2'
-        />
-        <button
-          onClick={handleSendMessage}
-          className='bg-blue-500 text-white rounded-r-lg p-2 hover:bg-blue-600'
-        >
-          Send
-        </button>
-      </div>
+      {renderRoomInfo()}
+      {renderSchedule()}
+      {renderMessages()}
+      {renderActionButtons()}
+      {renderMessageInput()}
 
       <RoomUserModal
         isOpen={isUserModalOpen}
@@ -157,8 +253,15 @@ export default function RoomPage({ roomId }) {
         usersInfo={usersInfo}
       />
       <ScheduleModal
+        roomId={roomId}
         isOpen={isScheduleModalOpen}
         onClose={() => setIsScheduleModalOpen(false)}
+        onScheduleUpdated={handleScheduleUpdated}
+      />
+      <RoomRequestModal
+        roomId={roomId}
+        isOpen={isRoomRequestModalOpen}
+        onClose={() => setIsRoomRequestModalOpen(false)}
       />
     </div>
   );
