3ca934ff3a1f895b4751ac3e539cc25cad8fb04b
HP657 <junlee5070@gmail.com>
2025-01-03 16:24:16 +0900

    토큰 굳이 필요 없을듯 해서 삭제

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -8,20 +8,14 @@ export default async function API(
 ) {
   const API_URL = process.env.REACT_APP_API_URL;
   let headers = {};
+
   if (requireToken) {
-    const token = await localStorage.getItem('accessToken');
-    try {
-      await axios.get(`${API_URL}/api/token/check`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-    } catch (error) {
-      console.error('Token check error: ', error);
+    const token = localStorage.getItem('accessToken');
+    if (!token) {
+      console.error('No access token found');
       return null;
     }
-
-    if (token) {
-      headers.Authorization = `Bearer ${token}`;
-    }
+    headers.Authorization = `Bearer ${token}`;
   }
 
   const url = `${API_URL}${endpoint}`;
