4fa5f5e81f2d62ee8ab46d4ff86d5bbfc10b5f9e
HP657 <junlee5070@gmail.com>
2025-01-03 16:28:15 +0900

    게시물 속성 변경

--- FrontEnd/src/components/Posts.js
+++ FrontEnd/src/components/Posts.js
@@ -10,44 +10,60 @@ export default function Posts({ posts }) {
   };
 
   return (
-    <ul>
-      {posts.map((post, index) => (
-        <li key={post.id}>
-          <Link to={`/post/${post.id}`}>
-            <h3 className='text-xl font-semibold flex items-center'>
-              {isNew(post.date) && (
-                <span className='inline-block bg-red-400 text-white text-xs rounded-full px-2 py-1 mr-2'>
-                  NEW
-                </span>
+    <>
+      {posts && posts.length > 0 ? (
+        <ul>
+          {posts.map((post, index) => (
+            <li key={post.postId}>
+              <Link to={`/post/${post.postId}`}>
+                <h3 className='text-xl font-semibold flex items-center'>
+                  {isNew(post.createdAt) && (
+                    <span className='inline-block bg-red-400 text-white text-xs rounded-full px-2 py-1 mr-2'>
+                      NEW
+                    </span>
+                  )}
+                  {post.title}
+                </h3>
+                <p className='text-gray-700'>
+                  {post.contents.length > 50
+                    ? `${post.contents.substring(0, 50)}...`
+                    : post.contents}
+                </p>
+                <div className='text-sm text-gray-500 mt-2'>
+                  <span>
+                    {post.author} | {new Date(post.createdAt).getFullYear()}-
+                    {(new Date(post.createdAt).getMonth() + 1)
+                      .toString()
+                      .padStart(2, '0')}
+                    -
+                    {new Date(post.createdAt)
+                      .getDate()
+                      .toString()
+                      .padStart(2, '0')}
+                  </span>
+                </div>
+              </Link>
+              {index < posts.length - 1 && (
+                <hr className='my-4 border-gray-300' />
               )}
-              {post.title}
-            </h3>
-            <p className='text-gray-700'>
-              {post.description.length > 50
-                ? `${post.description.substring(0, 50)}...`
-                : post.description}
-            </p>
-            <div className='text-sm text-gray-500 mt-2'>
-              <span>
-                {post.author} | {post.date}
-              </span>
-            </div>
-          </Link>
-          {index < posts.length - 1 && <hr className='my-4 border-gray-300' />}
-        </li>
-      ))}
-    </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>게시물이 없습니다.</p>
+      )}
+    </>
   );
 }
 
 Posts.propTypes = {
   posts: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      postId: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
+      contents: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
