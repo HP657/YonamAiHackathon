c18c083663155bc76bb96d566d237be535cff65d
HP657 <junlee5070@gmail.com>
2025-01-03 16:29:08 +0900

    토큰 체크

--- FrontEnd/src/components/TokenCheck.js
+++ FrontEnd/src/components/TokenCheck.js
@@ -1,15 +1,19 @@
-// import API from '../services/api';
-
-// export default async function CheckToken() {
-//   try {
-//     const response = await API('/api/token/check', 'GET', null, true);
-//     return response.data;
-//   } catch (error) {
-//     console.error('API 호출 중 에러 발생:', error);
-//     return false;
-//   }
-// }
+import API from '../services/api';
 
 export default async function CheckToken() {
-  return true;
+  try {
+    const response = await API('/api/token/check', 'GET', null, true);
+    if (response) {
+      return response.data;
+    } else {
+      console.error('Token check failed:', response);
+      return false;
+    }
+  } catch (error) {
+    console.error(
+      'API 호출 중 에러 발생:',
+      error.response ? error.response.data : error
+    );
+    return false;
+  }
 }
