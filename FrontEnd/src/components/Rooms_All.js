95eab19d450e5243e6d39808692461bf3382e44e
HP657 <junlee5070@gmail.com>
2025-01-03 16:28:58 +0900

    룸 가져오는 컴포넌트 수정

--- FrontEnd/src/components/Rooms_All.js
+++ FrontEnd/src/components/Rooms_All.js
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types';
+import API from '../services/api';
 
 export default function Rooms_All({ rooms }) {
-  const onClick = (roomTitle) => {
-    alert(`You clicked on: ${roomTitle}`);
+  const onClick = async (roomId) => {
+    await API(`/api/room/${roomId}/request`, 'POST', null, true);
+    alert('요청 성공');
   };
 
   return (
     <ul>
       {rooms.map((room, index) => (
-        <li key={room.id}>
+        <li key={room.roomId}>
           <button
-            onClick={() => onClick(room.title)}
+            onClick={() => onClick(room.roomId)}
             className='w-full text-left cursor-pointer bg-transparent'
             style={{ border: 'none', padding: '0', margin: '0' }}
           >
             <h3 className='text-xl font-semibold'>
-              {room.title} | {room.topic}
+              {room.name} | {room.topic}
             </h3>
             <p className='text-gray-700'>{room.description}</p>
             <div className='text-sm text-gray-500 mt-2'>
-              <span>방장: {room.leader}</span>
+              <span>방장: {room.owner?.username || '정보 없음'}</span>
             </div>
           </button>
           {index < rooms.length - 1 && <hr className='my-4 border-gray-300' />}
@@ -32,10 +34,16 @@ export default function Rooms_All({ rooms }) {
 Rooms_All.propTypes = {
   rooms: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
+      roomId: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      leader: PropTypes.string.isRequired,
+      owner: PropTypes.shape({
+        userId: PropTypes.number,
+        email: PropTypes.string,
+        username: PropTypes.string,
+        studentId: PropTypes.number,
+        gpa: PropTypes.number,
+      }),
       topic: PropTypes.string.isRequired,
     })
   ).isRequired,
--- FrontEnd/src/components/Rooms_User.js
+++ FrontEnd/src/components/Rooms_User.js
@@ -3,32 +3,40 @@ import { Link } from 'react-router-dom';
 
 export default function Rooms_User({ rooms }) {
   return (
-    <ul>
-      {rooms.map((room, index) => (
-        <li key={room.id}>
-          <Link to={`/room/${room.id}`}>
-            <h3 className='text-xl font-semibold'>
-              {room.title} | {room.topic}
-            </h3>
-            <p className='text-gray-700'>{room.description}</p>
-            <div className='text-sm text-gray-500 mt-2'>
-              <span>방장: {room.leader}</span>
-            </div>
-          </Link>
-          {index < rooms.length - 1 && <hr className='my-4 border-gray-300' />}
-        </li>
-      ))}
-    </ul>
+    <>
+      {rooms.length > 0 ? (
+        <ul>
+          {rooms.map((room, index) => (
+            <li key={room.roomId}>
+              <Link to={`/room/${room.roomId}`}>
+                <h3 className='text-xl font-semibold'>
+                  {room.name} | {room.topic}
+                </h3>
+                <p className='text-gray-700'>{room.description}</p>
+                <div className='text-sm text-gray-500 mt-2'>
+                  <span>방장: {room.owner.username}</span>
+                </div>
+              </Link>
+              {index < rooms.length - 1 && (
+                <hr className='my-4 border-gray-300' />
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>가입한 스터디가 없습니다.</p>
+      )}
+    </>
   );
 }
 
 Rooms_User.propTypes = {
   rooms: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
+      roomId: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      leader: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired,
       topic: PropTypes.string.isRequired,
     })
   ).isRequired,
