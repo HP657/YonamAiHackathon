83478f3b16389503463f60d86461a9d1eaa3722a
HP657 <junlee5070@gmail.com>
2025-01-03 16:29:54 +0900

    더미데이터 삭제

--- FrontEnd/src/pages/PostPage.js
+++ FrontEnd/src/pages/PostPage.js
@@ -1,61 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Posts from '../components/Posts';
+import API from '../services/api';
+import { useEffect, useState } from 'react';
+import CheckToken from '../components/TokenCheck';
 
 export default function PostListPage() {
-  const posts = [
-    {
-      id: 1,
-      title: 'Understanding Closures in JavaScript',
-      description: 'A deep dive into closures in JavaScript.',
-      author: 'John Doe',
-      date: '2024-12-27',
-    },
-    {
-      id: 2,
-      title: 'React Hooks: A Comprehensive Guide',
-      description: 'Everything you need to know about React Hooks.',
-      author: 'Jane Smith',
-      date: '2023-10-02',
-    },
-    {
-      id: 3,
-      title: 'JavaScript Fundamentals',
-      description: 'A comprehensive overview of JavaScript basics.',
-      author: 'Bob Johnson',
-      date: '2023-10-03',
-    },
-    {
-      id: 4,
-      title: 'Advanced React Concepts',
-      description: 'Exploring advanced topics in React.',
-      author: 'Alice Brown',
-      date: '2023-10-04',
-    },
-    {
-      id: 5,
-      title: 'Web Development Trends',
-      description: 'The latest trends and innovations in web development.',
-      author: 'Charlie Davis',
-      date: '2023-10-05',
-    },
-    {
-      id: 6,
-      title: 'Frontend Security Best Practices',
-      description: 'Securing your frontend applications with best practices.',
-      author: 'Emily Taylor',
-      date: '2023-10-06',
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const tokencheck = async () => {
+      const isValidToken = await CheckToken();
+      if (!isValidToken) {
+        navigate('/login');
+        return;
+      }
+
+      await fetchPosts();
+    };
+    tokencheck();
+
+    const fetchPosts = async () => {
+      const response = await API('/api/post/all', 'GET', null, true);
+      if (response && response.data) {
+        setPosts(response.data.data);
+      } else {
+        console.error('Failed to fetch posts:', response);
+        setPosts([]);
+      }
+    };
+  }, []);
 
   return (
     <div className='max-w-2xl mx-auto p-4'>
-      <h1 className='text-3xl font-bold mb-6'>게시물 목록</h1>
-      <Link
-        to='/add_post'
-        className='inline-block mb-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
-      >
-        Add New Post
-      </Link>
+      <h1 className='text-3xl font-bold mb-4 flex justify-between items-center'>
+        <span>게시물 목록</span>
+        <Link to='/post/add' className='text-black text-xl'>
+          작성하기
+        </Link>
+      </h1>
       <ul>
         <Posts posts={posts} />
       </ul>
