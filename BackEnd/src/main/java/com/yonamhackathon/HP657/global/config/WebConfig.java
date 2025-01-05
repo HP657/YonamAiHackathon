7def96f6bc034599149dd9901ac955939420984c
HP657 <junlee5070@gmail.com>
2025-01-05 19:04:01 +0900

    모든 도메인 CORS 허용

--- BackEnd/src/main/java/com/yonamhackathon/HP657/global/config/WebConfig.java
+++ BackEnd/src/main/java/com/yonamhackathon/HP657/global/config/WebConfig.java
@@ -5,15 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
 }
